95ec661880b20399aad60adb95c64796e92b3163
madsonmms <69799601+4control-suporte@users.noreply.github.com>
2022-10-16 12:28:41 -0300

    Teste de botão, no momento o texto é alterado quando se clica na primeira opção

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  /*   state = { texto: 'texto1' } */
+
+  /*   handlerPClick = (event) => {
+      event.preventDefault();
+      this.setState({ texto: 'texto2' });
+    } */
+
+  handlerChangeText = (event) => {
+    event.preventDefault();
+    const getValue = event.target.value;
+    console.log(getValue);
+  }
+
+  render() {
+
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className="dropdown">
+            <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Muda Texto
+            </button>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" value='1' href="#" onClick={(e) => (this.handlerChangeText(e))}>Texto</a></li>
+              <li><a className="dropdown-item" value='2' href="#"></a></li>
+              <li><a className="dropdown-item" value='3' href="#"></a></li>
+            </ul>
+          </div>
+        </div>
+        <div className='row'>
+        </div>
+      </div>
+    );
+
+  }
+
 }
 
 export default App;
