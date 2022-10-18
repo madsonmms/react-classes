d25b697421cac7fa9b595564323cf663e1c95028
madsonmms <madson.mms@gmail.com>
2022-10-18 17:00:18 -0300

    Corrigindo comentários

--- src/components/Pessoa.js
+++ src/components/Pessoa.js
@@ -1,5 +1,7 @@
 import styles from "./Pessoa.module.css";
 
+//({nome, idade...}) é utilizado para desestruturar props, para que não seja necessário iniciar props.name,
+// props.idade en todas as partes da função
 function Pessoa({ nome, idade, profissao, foto }) {
 	return (
 		<div className="container">
@@ -12,7 +14,9 @@ function Pessoa({ nome, idade, profissao, foto }) {
 				<p>Idade: {idade}</p>
 				<p>Profissão: {profissao}</p>
 
-				{/* Na intenção de usar classes bootstrap combinadas com classes customizadas no JSX é necessário utilizar a crase (back-ticks) para definir que dentro dessa propriedade className haverá texto comum e variáveis
+				{/* Na intenção de usar classes bootstrap combinadas com classes customizadas no JSX é 
+                necessário utilizar a crase (back-ticks) para definir que dentro dessa propriedade className 
+                haverá texto comum e variáveis.
                 Além disso as variáveis deverão ser acompanhadas de um cifrão e colchetes.  */}
 				<button className={`${styles.botao} btn btn-danger`}>Botão</button>
 			</div>
